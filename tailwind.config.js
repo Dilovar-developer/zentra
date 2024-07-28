e07e2d425b223d8bf29b323eb52a3b858f4d573e
Dilovar-developer <dilovardilovar059@gmail.com>
2024-07-28 19:33:54 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'hero-pattern':"url('https://img.freepik.com/free-photo/top-view-office-desk-with-growth-chart-copy-space_23-2148780606.jpg?ga=GA1.1.1596078874.1721475565&semt=ais_user')"
+    },
+  },
+  plugins: [],
+}
+}
